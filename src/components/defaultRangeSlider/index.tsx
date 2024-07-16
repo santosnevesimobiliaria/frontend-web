1af0c8335ca4a4348b5387237c5615f5fcf9c263
Emmanoel Coutinho <coutinho.codes@gmail.com>
2024-07-16 13:39:24 -0300

    nova range slider

---
+++ src/components/defaultRangeSlider/index.tsx
@@ -0,0 +1,67 @@
+import { defaultColors } from '@/constants/styles/defaultColors';
+import {
+  RangeSlider,
+  RangeSliderFilledTrack,
+  RangeSliderMark,
+  RangeSliderThumb,
+  RangeSliderTrack,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+
+function DefaultRangeSlider() {
+  const [sliderValue, setSliderValue] = useState([0, 1000000]);
+
+  return (
+    <RangeSlider
+      aria-label={['min', 'max']}
+      defaultValue={[0, 1000000]}
+      min={0}
+      max={1000000}
+      step={10000}
+      onChange={(val) => setSliderValue(val)}
+    >
+      {/* <RangeSliderMark value={250000} mt="1" ml="-2.5" fontSize="sm">
+        R$ 250.000
+      </RangeSliderMark>
+      <RangeSliderMark value={500000} mt="1" ml="-2.5" fontSize="sm">
+        R$ 500.000
+      </RangeSliderMark>
+      <RangeSliderMark value={750000} mt="1" ml="-2.5" fontSize="sm">
+        R$ 750.000
+      </RangeSliderMark> */}
+      <RangeSliderMark
+        value={sliderValue[0]}
+        textAlign="center"
+        bg={defaultColors.purple}
+        color="white"
+        mt="-10"
+        ml="-5"
+        w="24"
+      >
+        R$ {sliderValue[0].toLocaleString('pt-BR')}
+      </RangeSliderMark>
+      <RangeSliderMark
+        value={sliderValue[1]}
+        textAlign="center"
+        bg={defaultColors.purple}
+        color="white"
+        mt="-10"
+        ml="-5"
+        w="24"
+      >
+        R$ {sliderValue[1].toLocaleString('pt-BR')}
+      </RangeSliderMark>
+      <RangeSliderTrack bg="red.100">
+        <RangeSliderFilledTrack bg={defaultColors.purple} />
+      </RangeSliderTrack>
+      <RangeSliderThumb boxSize={6} index={0}>
+        {/* <Box color="tomato" as={MdGraphicEq} /> */}
+      </RangeSliderThumb>
+      <RangeSliderThumb boxSize={6} index={1}>
+        {/* <Box color="tomato" as={MdGraphicEq} /> */}
+      </RangeSliderThumb>
+    </RangeSlider>
+  );
+}
+
+export default DefaultRangeSlider;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,9 @@
 import DefaultSelect from '@/components/deafultSelect';
 import DefaultButton from '@/components/defaultButton';
+import DefaultRangeSlider from '@/components/defaultRangeSlider';
 import DefaultTextInput from '@/components/defaultTextInput';
 import { randomPicture } from '@/utils/randomPicture';
-import {
-  Image,
-  SimpleGrid,
-  Switch,
-} from '@chakra-ui/react';
+import { Image, SimpleGrid, Switch } from '@chakra-ui/react';
 
 export default function Home() {
   return (
@@ -21,7 +18,7 @@ export default function Home() {
         />
       </div>
 
-      <div className="absolute top-[600px] left-[50%] transform -translate-x-1/2 flex flex-col mx-auto items-center py-3 px-4 w-full max-w-[50%] h-96 bg-orange-600 text-[#FBFBFA] rounded-lg">
+      <div className="absolute top-[600px] left-[50%] transform -translate-x-1/2 flex flex-col mx-auto items-center py-3 px-4 w-full max-w-[50%] h-[420px] bg-orange-600 text-[#FBFBFA] rounded-lg">
         <span className="text-3xl font-medium mt-4">
           Encontre o Imóvel dos seus sonhos
         </span>
@@ -34,16 +31,27 @@ export default function Home() {
           />
         </span>
         <span className="w-full h-[2px] bg-white" />
-        <SimpleGrid className="w-full mt-4" columns={3} spacing={10}>
+        <SimpleGrid className="w-full mt-4" columns={3} spacingY={10} spacingX={3}>
           <DefaultSelect options={[]} placeholder="Tipo de Imóvel" />
           <DefaultSelect options={[]} placeholder="Cidade" />
           <DefaultSelect options={[]} placeholder="Bairro" />
-          <DefaultTextInput placeholder="Valor Mínimo" />
-          <DefaultTextInput placeholder="Valor Máximo" />
-          <div className='flex flex-col' >
-            <span>Apenas Financiavéis?</span>
-            <Switch colorScheme='green' />
+          <div>
+            <span className="font-medium text-lg">
+              Selecione a faixa de Preço
+            </span>
+            <div className="flex flex-col justify-center items-center w-full h-28 px-4">
+              <DefaultRangeSlider />
+            </div>
           </div>
+          <div className="flex flex-col items-center w-full h-28">
+            <div className="flex flex-col gap-4">
+              <span className="font-medium text-lg">Apenas Financiavéis?</span>
+              <Switch size="lg" colorScheme="purple" />
+            </div>
+          </div>
+          <span className='pt-6 w-full h-full' >
+            <DefaultButton text="Buscar Imóvel" isSearchButton maxWidth={300} />
+          </span>
         </SimpleGrid>
       </div>
     </div>
